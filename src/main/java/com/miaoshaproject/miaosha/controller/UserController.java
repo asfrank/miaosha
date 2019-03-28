8733dc21f4467dd26ce5449b504a38739ca32304
asfrank <asfrank@qq.com>
2019-03-28 14:55:16 +0800

    用户模型管理--otp验证码获取

--- src/main/java/com/miaoshaproject/miaosha/controller/UserController.java
+++ src/main/java/com/miaoshaproject/miaosha/controller/UserController.java
@@ -8,13 +8,12 @@ import com.miaoshaproject.miaosha.service.UserService;
 import com.miaoshaproject.miaosha.service.model.UserModel;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Random;
+
 
 @Controller("user")
 @RequestMapping("/user")
@@ -22,6 +21,26 @@ public class UserController extends BaseController{
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    @RequestMapping("/getotp")
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name = "telephone")String telephone){
+        //需要按照一定的规则生成otp验证码
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);   //[0,99999)
+        randomInt += 10000;
+        String otpCode = String.valueOf(randomInt);
+
+        //将otp验证码同对应的手机号相关联，使用httpsession的方式绑定code和手机号
+        httpServletRequest.getSession().setAttribute(telephone, otpCode);
+
+        //todo:将otp验证码通过短信通道发送给用户
+
+        return CommonReturnType.create(null);
+    }
 
     @RequestMapping("/get")
     @ResponseBody
