324c19a7f1f296b50b2a8c691c9f06a79ecb5033
asfrank <asfrank@qq.com>
2019-03-29 17:45:27 +0800

    商品模型--商品列表

--- src/main/java/com/miaoshaproject/miaosha/service/impl/ItemServiceImpl.java
+++ src/main/java/com/miaoshaproject/miaosha/service/impl/ItemServiceImpl.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ItemServiceImpl implements ItemService {
@@ -74,7 +75,14 @@ public class ItemServiceImpl implements ItemService {
 
     @Override
     public List<ItemModel> listItem() {
-        return null;
+        List<ItemDO> itemDOList = itemDOMapper.listItem();
+        //将里面的每一个itemdo map成 itemmodel
+        List<ItemModel> itemModelList = itemDOList.stream().map(itemDO -> {
+            ItemStockDO itemStockDO = itemStockDOMapper.selectByItemId(itemDO.getId());
+            ItemModel itemModel = this.convertModelFromDataObject(itemDO, itemStockDO);
+            return itemModel;
+        }).collect(Collectors.toList());
+        return itemModelList;
     }
 
     @Override
