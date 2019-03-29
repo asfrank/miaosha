01caaa5212d4260a3e146b5d077eaec55a467cfc
asfrank <asfrank@qq.com>
2019-03-29 19:48:57 +0800

    交易模型--模型创建

---
+++ src/main/java/com/miaoshaproject/miaosha/service/impl/OrderServiceImpl.java
@@ -0,0 +1,11 @@
+package com.miaoshaproject.miaosha.service.impl;
+
+import com.miaoshaproject.miaosha.service.OrderService;
+import com.miaoshaproject.miaosha.service.model.OrderModel;
+
+public class OrderServiceImpl implements OrderService {
+    @Override
+    public OrderModel createOrder(Integer userId, Integer itemId, Integer amount) {
+        return null;
+    }
+}
