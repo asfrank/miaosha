16ba0224da0c4b16db32674d2b8987269e978706
asfrank <asfrank@qq.com>
2019-03-29 19:37:18 +0800

    交易模型--模型创建

---
+++ src/main/java/com/miaoshaproject/miaosha/service/model/OrderModel.java
@@ -0,0 +1,69 @@
+package com.miaoshaproject.miaosha.service.model;
+
+import java.math.BigDecimal;
+
+//用户下单的交易模型
+public class OrderModel {
+    //2018898978789
+    private String id;
+
+    private Integer userId;
+
+    private Integer itemId;
+    //购买商品的单价
+    private BigDecimal itemPrice;
+
+    //购买数量
+    private Integer amount;
+
+    //购买总金额
+    private BigDecimal orderPrice;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(BigDecimal orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public BigDecimal getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(BigDecimal itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+}
